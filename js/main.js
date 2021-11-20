80ea1a5d48010609b2ce9b31fdaf649240ee6b81
nic0mp <nico.mp215@gmail.com>
2021-11-19 23:19:00 -0600

    js

---
+++ js/main.js
@@ -0,0 +1,6 @@
+const weatherKey = "0f599ff9a61ff8b159247255d3361ca1"
+
+//Function to get city name from form, pass it to the API call, and fill global variable with data
+async function getData(){
+    
+}
