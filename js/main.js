ac6e28088fa1bb6cd6ee8a8ead90d86511bbd3dc
nic0mp <nico.mp215@gmail.com>
2021-12-06 13:53:54 -0600

    updates

--- js/main.js
+++ js/main.js
@@ -1,5 +1,10 @@
 const weatherKey = "0f599ff9a61ff8b159247255d3361ca1"
 
+roundNum = (num) => {
+    let rounded = Math.round(num*10)/10;
+    return rounded
+}
+
 //Function to get city name from form, pass it to the API call, and fill global variable with data
 async function getData(){
     cityName = document.getElementById('city-name').value;
@@ -8,9 +13,9 @@ async function getData(){
     data = await response.json();
     document.getElementById("city").innerHTML=data.name;
     document.getElementById("country").innerHTML= data.sys.country
-    document.getElementById("high").innerHTML = `${data.main.temp_max} F`;
-    document.getElementById("low").innerHTML =`${data.main.temp_min} F`;
-    document.getElementById("feel").innerHTML =`${data.main.temp} F`;
+    document.getElementById("high").innerHTML = `${roundNum(data.main.temp_max)} F`;
+    document.getElementById("low").innerHTML =`${roundNum(data.main.temp_min)} F`;
+    document.getElementById("feel").innerHTML =`${roundNum(data.main.feels_like)} F`;
     document.getElementById("description").innerHTML=data.weather[0].description;
     document.getElementById("cloud").innerHTML = `${data.clouds.all} %`;
     return data
